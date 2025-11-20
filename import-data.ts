a5b74a4d0e0791cc763aae2cb6af19deb8ed43ed
melialbert <melialbert887@gmail.com>
2025-11-20 14:37:01 +0100

    Updated import-data.ts

--- import-data.ts
+++ import-data.ts
@@ -1,5 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 import * as fs from 'fs';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const supabaseUrl = process.env.VITE_SUPABASE_URL || '';
 const supabaseKey = process.env.VITE_SUPABASE_ANON_KEY || '';
